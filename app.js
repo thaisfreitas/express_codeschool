7384305170db6a8148097fb1162006e0e756528d
Thais Freitas <tfreitas@thoughtworks.com>
2017-08-02 11:13:08 -0300

    add cities route

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+//level 1 - locations
 var express = require('express');
 var app = express();
 
-app.get('/locations', function(request, response){
+app.get('/cities', function(request, response){
   var city = ["Caspiana", "Indigo", "Paradise"];
   response.send(city);
+});
+
+//cities
+app.get('/locations', function(request, response) {
+  response.redirect(301, '/cities');
 });
 
 app.listen(3001, function() {
