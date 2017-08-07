477fb7e0e77cf6ba5b6eb1a5620c688cd4587652
Thais Freitas <tfreitas@thoughtworks.com>
2017-08-07 10:49:56 -0300

    add client file

---
+++ client.js
@@ -0,0 +1,14 @@
+//script tags
+
+$(function(){
+
+  $.get('/cities', appendToList );
+
+  function appendToList(cities) {
+    var list = [];
+    for(var i in cities){
+      list.push($('<li>', { text: cities[i] }));
+    }
+    $('.city-list').append(list);
+  }
+});
