9da21d167ab8a48ff9e368e5c1a9102cc278e3ba
Thais Freitas <tfreitas@thoughtworks.com>
2017-08-07 19:19:27 -0300

    add validation of post params

--- postRequest.js
+++ postRequest.js
@@ -21,3 +21,21 @@ var createCity = function(name, description){
   cities[name] = description;
   return name;
 };
+
+//validation
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
+app.post('/cities', parseUrlencoded, function (request, response) {
+  if( request.body.description.length > 4) {
+    var city = createCity(request.body.name, request.body.description);
+    response.status(201).json(city);
+  } else {
+    response.status(400).json('Invalid City');
+  }
+});
+
+app.listen(3000);
