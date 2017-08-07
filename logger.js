64108970b3fa1cad02ca2fe8b39cc86861de3dad
Thais Freitas <tfreitas@thoughtworks.com>
2017-08-07 11:11:48 -0300

    add logging file

---
+++ logger.js
@@ -0,0 +1,13 @@
+//logging middleware
+module.exports = function (request, response, next) {
+  var startTime = +new Date();
+  var stream = process.stdout;
+  var duration = null;
+
+  response.on('finish', function () {
+    duration = +new Date() - startTime;
+    stream.write("This request took " + duration + " ms");
+  });
+  next();
+
+};
