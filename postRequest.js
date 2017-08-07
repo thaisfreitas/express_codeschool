104c0b2b2e3374ff8d8b734f99590fd475e785d8
Thais Freitas <tfreitas@thoughtworks.com>
2017-08-07 19:14:34 -0300

    add parser setup

---
+++ postRequest.js
@@ -0,0 +1,23 @@
+//to install body parser
+//npm install body-parser
+
+
+//section 1 - parser setup
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
+
+app.post('/cities', parseUrlencoded, function (request, response) {
+  var city = createCity(request.body.name,request.body.description );
+  response.status(201);
+  response.json(city);
+});
+
+app.listen(3000);
+
+var createCity = function(name, description){
+  cities[name] = description;
+  return name;
+};
